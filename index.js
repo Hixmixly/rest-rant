45c9a73bcf82d5e11fd713a82fe96b1a81b666ce
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-12 21:39:26 -0700

    all fixed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
-const express = require ('express')
-const app = express ()
+const express = require('express')
+const app = express()
 
-app.get ('/', (req, res) => {
-    res.send ('Hello world!')
+app.use('/places', require('./controller/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
 
-app.get('*', (req, rex) => {
-    res.send ('<h1>404</h1> ') 
-}) 
-
-
-app.listen (process.env.PORT)
+app.listen(process.env.PORT)
