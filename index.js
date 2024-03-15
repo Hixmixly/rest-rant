26788b20f06597660b1b9f4516aba54822acf475
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-14 20:39:43 -0700

    fixed homeview

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set ('view engine', 'jsx')
-app.engine ('jsx', require ('express-react-views').createEngine)
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
-app.use('/places', require('./controller/places'))
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.render('error404')
+  res.send('404 page')
 })
 
 app.listen(process.env.PORT)
