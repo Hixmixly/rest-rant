28c86a7711c8ad68e29f77d33ae48ead8ad7e2e5
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-21 19:34:40 -0700

    added models/places.js

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,11 @@
 const router = require ('express').Router()
+const places = require ('../models/places.js')
 
 // GET /places
 router.get('/', (req, res) => {
-    res.render('places/index')
-  })
   
-  
+res.render('places/index', {places})
 
-    module.exports = router
+    })
+    
+module.exports = router
