1bc29f9e96f20380e400712318bffaad0be7a9b2
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-21 20:16:49 -0700

    added css

--- views/default.jsx
+++ views/default.jsx
@@ -5,7 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
+                <link rel="stylesheet" href= "/public/css/style.css" />
+
             </head>
             <body>
                 {html.children}
@@ -16,6 +17,3 @@ function Def (html) {
 
 module.exports = Def
 
-}
-
-module.exports = Def
