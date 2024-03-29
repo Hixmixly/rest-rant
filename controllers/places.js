335e996a0360df05f7507932fb31496bb1de0f82
Hixmixly <152249445+Hixmixly@users.noreply.github.com>
2024-03-28 21:37:48 -0700

    added delete and edit button

--- controllers/places.js
+++ controllers/places.js
@@ -1,23 +1,24 @@
 const router = require ('express').Router()
 const places = require ('../models/places.js')
 
+  
 //get places/new
 router.get('/new', (req, res) => {
-    res.render('places/new')
+    res.render('places/new', {place: places[id] })
   })
 
-  //POST /places
-  router.post('/', (req, res) => {
-    console.log(req.body)
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
+//POST /places
+router.post('/', (req, res) => {
+ console.log(req.body)
+   if (!req.body.pic) {
+     // Default image if one is not provided
+     req.body.pic = 'http://placekitten.com/400/400'
+   }
+   if (!req.body.city) {
+     req.body.city = 'Anytown'
+   }
+   if (!req.body.state) {
+     req.body.state = 'USA'
     }
 
     places.push(req.body)
@@ -31,5 +32,19 @@ router.get('/', (req, res) => {
 res.render('places/index', {places})
 
     })
+
+    //Get places/show
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/show', {place: places [id] })
+    }
+  })
     
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,7 +6,36 @@ function show (data) {
         <Def>
           <main>
             <h1>{data.place.name}</h1>
-            
+            <body>
+                
+                <div>
+                    <h3>Rating</h3>
+                    <p>Not Rated</p>
+                </div>
+                
+                <div>
+                    <h3>Description</h3>
+                    <p></p>
+                </div>
+                
+                <div>
+                    <h3>Comments</h3>
+                    <p></p>
+                </div>
+               
+                <div>
+                    <a href= {`/places/${data.id}/edit`} className="btn btn-warning"> 
+                         Edit
+                    </a> 
+                </div>
+               
+                <form method='POST' action={`/places/${data.id}?_method=DELETE`}>
+                    <button type= 'submit' className='btn btn-danger'>
+                    Delete
+                    </button>
+                </form>
+
+            </body>
           </main>
         </Def>
     )
